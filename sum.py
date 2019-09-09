f134cfe7b949eee24459e52e4d1a6b97f3fd865b
Maria Rayane Alves <mrayalves05@gmail.com>
2019-09-09 00:45:16 -0300

    Init

---
+++ sum.py
@@ -0,0 +1,41 @@
+import threading
+
+lock = threading.Lock() 
+
+def sumthread (nrow, rowa, rowb):
+  global lock
+  global result
+  aux = []
+  
+  for i in range(len(rowa)):
+    aux.append(rowa[i]+rowb[i])
+  
+  lock.acquire()
+  result.insert(nrow, aux)  
+  lock.release()
+  
+def unroll(args, function, method, result):
+  if method == "thre":
+    a = args[0]
+    b = args[1]
+    
+    threads = []
+    row = len(a)    
+    
+    for i in range(row):  
+      threads.insert(i, threading.Thread(target=function, args=(i, a[i], b[i])))
+      threads[i].start()
+
+
+if __name__ == '__main__':
+  a = [[1, 2, 5], [5, 8, 9]]
+  b = [[3, 2, 1], [5, 9, 6]]
+  result = []
+
+  args= []
+  args.insert(0, a)
+  args.insert(1, b)
+
+  unroll(args, sumthread, "thre", result)
+
+  print(result)
